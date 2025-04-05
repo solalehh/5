d5619a7a0c244a0e4821a9903456ed7debdae175
solalehh <farmanisolaleh@gmail.com>
2025-04-05 13:18:57 +0330

    Add files via upload

---
+++ دپ.py
@@ -0,0 +1,13 @@
+score=int(input("Enter your score: "))
+if 90<=score<=100:
+    print("A+")
+elif 70<=score<=90:
+     print("A")
+elif 50<=score<=70:
+    print("B+")
+elif 30<=score<=50:
+    print("B")
+elif 10<=score<=30:
+    print("c")
+else:
+    print("rejected")
